2acc0add41244f98ee2237a2489c86aa3e1e16d9
arafnishan <mahmud.nishan@gmail.com>
2021-07-25 17:24:32 -0400

    product slider 01 commited

--- js/main.js
+++ js/main.js
@@ -1,4 +1,45 @@
 (function ($) {
     "use strict";
+    /*-----------------------------
+   Product Slider One Carousel
+   ------------------------------*/
+
+    $('.product-slider-01').slick({
+        dots: false,
+        arrows: true,
+        prevArrow: "<img class='product-slider-01-left-arrow' src='img/product-slider-01/left-arrow.png'>",
+        nextArrow: "<img class='product-slider-01-right-arrow' src='img/product-slider-01/right-arrow.png'>",
+        autoplay: false,
+        speed: 800,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false
+                }
+            }
+
+        ]
+    });
 
 })(jQuery);

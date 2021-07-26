dc269341211467a8b09560c02bc5f2fe6d6558f6
arafnishan <mahmud.nishan@gmail.com>
2021-07-25 23:50:24 -0400

    navigation done

--- js/main.js
+++ js/main.js
@@ -82,4 +82,59 @@ Hero Banner Slider
         ]
     });
 
+    /*----------------------------
+    jQuery Mean Menu
+    ------------------------------ */
+    $("#mobile-menu").meanmenu({
+        meanMenuContainer: ".mobile-menu",
+        meanScreenWidth: "767"
+    });
+    $(".info-bar").on("click", function () {
+        $(".extra-info").addClass("info-open");
+    });
+    $(".close-icon").on("click", function () {
+        $(".extra-info").removeClass("info-open");
+    });
+    /*----------------------------
+    Sticky menu active
+    ------------------------------ */
+    function fixed_top_menu() {
+        var windows = $(window);
+        windows.on("scroll", function () {
+            var header_height = $(".main-navigation").height();
+            var scrollTop = windows.scrollTop();
+            if (scrollTop > header_height) {
+                $(".main-navigation").addClass("sticky");
+            } else {
+                $(".main-navigation").removeClass("sticky");
+            }
+        });
+    }
+    fixed_top_menu();
+
+    $(function() {
+
+        $('.search-input input').blur(function() {
+
+            if ($(this).val()) {
+                $(this)
+                    .find('~ label, ~ span:nth-of-type(n+3)')
+                    .addClass('not-empty');
+            } else {
+                $(this)
+                    .find('~ label, ~ span:nth-of-type(n+3)')
+                    .removeClass('not-empty');
+            }
+        });
+
+        $('.search-input input ~ span:nth-of-type(4)').click(function() {
+            $('.search-input input').val('');
+            $('.search-input input')
+                .find('~ label, ~ span:nth-of-type(n+3)')
+                .removeClass('not-empty');
+        });
+
+        console.log('Hello from console!');
+
+    });
 })(jQuery);
